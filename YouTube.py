81c2824af8b369ffaecafb42b321eeb7b10e1342
Loukious <Loukious@users.noreply.github.com>
2024-08-08 09:56:27 +0100

    chore: Add GitHub Actions workflow for daily YouTube music playlist update

---
+++ YouTube.py
@@ -0,0 +1,74 @@
+import os
+import base64
+import json
+from ytmusicapi import YTMusic
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Decode the base64-encoded oauth.json file
+oauth_json_base64 = os.getenv('OAUTH_JSON_BASE64')
+oauth_json = base64.b64decode(oauth_json_base64).decode('utf-8')
+
+# Write the decoded oauth.json to a file
+with open('oauth.json', 'w') as f:
+    f.write(oauth_json)
+
+yt = YTMusic('oauth.json')
+
+def get_jams_from_item_shop():
+    url = "https://fortnite-api.com/v2/shop"
+    result = {
+        "hash": "",
+        "tracks": []
+    }
+    with requests.session() as s:
+        response = s.get(url)
+        data = response.json()
+    if data["status"] == 200:
+        result["hash"] = data["data"]["hash"]
+        seen_titles = set()
+        for item in data["data"]["entries"]:
+            if item["tracks"]:
+                for track in item["tracks"]:
+                    if track["title"] not in seen_titles:
+                        result["tracks"].append({
+                            "title": track["title"],
+                            "artist": track["artist"],
+                            "album": track["album"],
+                            "releaseYear": track["releaseYear"]
+                        })
+                        seen_titles.add(track["title"])
+    return result
+
+def search_and_add_tracks_to_playlist(playlist_id, tracks):
+    videos = []
+    for track in tracks:
+        search_results = yt.search(f"{track['artist']} {track['title']} {track.get('album', '')} {track.get('releaseYear', '')}")
+        if search_results:
+            videos.append(search_results[0]['videoId'])
+            print(f"Added {track['title']} by {track['artist']} to the playlist.")
+        else:
+            print(f"Could not find {track['title']} by {track['artist']} on YouTube.")
+    
+    yt.add_playlist_items(playlist_id, videos)
+
+def save_oauth_json():
+    with open('oauth.json', 'r') as f:
+        updated_oauth_json = f.read()
+    updated_oauth_json_base64 = base64.b64encode(updated_oauth_json.encode('utf-8')).decode('utf-8')
+    return updated_oauth_json_base64
+
+if __name__ == "__main__":
+    playlist_id = os.getenv("PLAYLIST_ID")
+    tracks = get_jams_from_item_shop()
+    playlist_items = yt.get_playlist(playlist_id)
+    videos = [{"videoId" : item['videoId'], "setVideoId": item['setVideoId']} for item in playlist_items['tracks']]
+    if videos:
+        yt.remove_playlist_items(playlist_id, videos=videos)
+    search_and_add_tracks_to_playlist(playlist_id, tracks["tracks"])
+    print("Finished adding tracks to the playlist.")
+    
+    updated_oauth_json_base64 = save_oauth_json()
+    print(f"::set-output name=OAUTH_JSON_BASE64::{updated_oauth_json_base64}")
